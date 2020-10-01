9e7422929b94144b78b173f687c301bfc0ca1a23
Dominic Bridgette <54685761+Afrodo1@users.noreply.github.com>
2020-09-30 23:44:06 -0700

    fixed the binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -7,24 +7,30 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-    curr_index = len(arr)//2
-    cut = arr
-    if cut[curr_index] == target:
-        return curr_index
-    else:
-        if cut[curr_index] > target:
-            cut = cut[slice(0,len(cut)//2)]
-            binary_search(cut,target)
-        if cut[curr_index] < target:
-            cut = cut[slice(curr_index,len(cut)-1)]
-            binary_search(cut,target)
     # Your code here
+    start = 0
+    end = (len(arr)-1)
+    switch = True
+    while switch is True:
+        middle = (end + start)//2
+        if arr == []:
+            return -1
+        if arr[middle]== target:
+            switch = False
+            return middle
+        else:
+            if start == end and target != arr[middle]:
+                return -1
+            elif target < arr[middle]:
+                end = middle
+            else:
+                start = middle + 1
 
 
 
 a = [1,2,3,4,5,6,7,8,9]
 
-print(binary_search(a,1))
+print(binary_search(a,9))
 
 # half_a = slice(0, len(a)//2)
 
